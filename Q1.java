803ad842eb1f3bcadeed614671b45f732c81f326
ArjunVijayaraja <108385083+ArjunVijayaraja@users.noreply.github.com>
2024-07-25 23:49:40 +0530

    Add files via upload

--- Q1.java
+++ Q1.java
@@ -10,9 +10,19 @@ public class Q1 {
     //writing a program using map() method to convert a list of Strings into uppercase.
 
     public static void main(String[] args) {
-        List<String> lst = Arrays.asList("aBc","d","ef");
-        System.out.println("The values in the 'lst' Array : "+lst);
-        List<String> upperNames = lst.stream()
+
+        //Here the list is converted to Stream and then the by using stream the strinfs are converted to uppercase
+//        List<String> lst = Arrays.asList("aBc","d","ef");
+//        System.out.println("The values in the 'lst' Array : "+lst);
+//        List<String> upperNames = lst.stream()
+//                .map(s-> s.toUpperCase())
+//                .collect(Collectors.toList());
+//        System.out.println("After using STREAMS along with map() function");
+//        System.out.println(upperNames);
+
+
+        //By passing the strings directly to a stream--and processing using map function to convert into uppercase and stored in a list.
+        List<String> upperNames = Stream.of("aBc","d","ef")
                 .map(s-> s.toUpperCase())
                 .collect(Collectors.toList());
         System.out.println("After using STREAMS along with map() function");
--- Q2.java
+++ Q2.java
@@ -9,6 +9,7 @@ public class Q2 {
     //to Print a list with non-empty strings
     public static void main(String[] args) {
         List<String> ListString = Arrays.asList("abc","","bc","efg","abcd","","jkl");
+        //The list is converted to a stream and by using filter method the empty names are removed and stored in a new list
         List<String> nonEMptyLst = ListString.stream()
                 .filter(( s) ->   s.length()>0)
                 .collect(Collectors.toList());
