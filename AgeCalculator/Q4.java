8a02ae4d2749ef3e1c6e75e4a54f4682ec629b3f
ArjunVijayaraja <108385083+ArjunVijayaraja@users.noreply.github.com>
2024-07-31 18:03:04 +0530

    Update Q4.java

--- AgeCalculator/Q4.java
+++ AgeCalculator/Q4.java
@@ -9,21 +9,26 @@ import java.util.Scanner;
 public class Q4 {
 
 	public static void main(String[] args) {
+		//Creating the scanner class
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter your Birthdate (yyyy-mm-dd): ");
+		//getting the Birthdate as an input from the user as a string
 		String b_Date = scanner.nextLine();
-		// LocalDate currdate = LocalDate.parse(LocalDate.now());
+		//writing the logic inside the try block - to handle the error in case an invaild number/date is provided
 		try {
+			//converting the String to LocalDate format
 			LocalDate ld_bDate = LocalDate.parse(b_Date);
-			// System.out.println(ld_bDate);
-			// ChronoPeriod.between(LocalDate.now().format(null), ld_bDate);
-			// System.out.println(LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE));
+			//Getting the current date using LocalDate --formatter is used to get only the date
 			String curDate = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+			//converting the Currentdate to the LocalDate format
 			LocalDate cDate = LocalDate.parse(curDate);
+			//Creating a period variable and passing the birthdate and local date -- for further calculations
 			Period period = Period.between(ld_bDate, cDate);
+			//storing the years, months and date in the below variables
 			int years = period.getYears();
 			int mon = period.getMonths();
 			int day = period.getDays();
+			//this is the output
 			System.out.println(years + " years " + mon + " months " + day + " Days");
 		} catch (Exception e) {
 			System.out.println("Enter a valid date ...!");
